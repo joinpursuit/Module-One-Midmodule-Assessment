7112e869365a9bdb1edccb7d20cd8d85e07b2fe4
Nora Ahmed <noraahmed@pursuit.org>
2020-12-22 12:39:14 -0500

    Incomplete

--- problems/2.functions.js
+++ problems/2.functions.js
@@ -2,17 +2,21 @@
 * @param {string} name - The name of the user to greet
 * @returns {null}
 */
-function logGreeting() {
-
+function logGreeting(name) {
+  let name = Nora
+  console.log("Hello ${name}!")
 }
+
 
 /** Returns a custom greeting string in the form "Hello Avery!"
 * @param {string} name - The name of the user to greet
 * @returns {string} - The custom greeting
 */
-function greeting() {
-
+function greeting(name) {
+  return "Hello " + "name" + "!"
 }
+console.log(greeting(Nora))
+
 
 /** Adds a number to a global variable called globalCount
  * Access globalCount like this: global.globalCount
@@ -20,18 +24,24 @@ function greeting() {
 * @returns {null}
 * @modifies {global.globalCount}
 */
-function incrementGlobalCount() {
 
+function incrementGlobalCount(n) {
+  let global = n++
 }
+
 /** Multiplies three numbers
 * @param {number} firstNum - The first number
 * @param {number} secondNum - The second number
 * @param {number} thirdNum - The third number
 * @returns {number} - The product of the three numbers
 */
-function multiply() {
-
+function multiply(n1, n2, n3) {
+ firstNumber = n1
+ secondNum = n2
+ thirdNum = n3
+return n1*n2*n3
 }
+console.log(multiply(n1 ,n2, n3))
 
 module.exports = {
   logGreeting,
