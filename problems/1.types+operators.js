3ae83b946581b6788b42ec5bb718087e20470646
Jarrett Harris <Jarrettharris@pursuit.org>
2020-12-22 12:47:16 -0500

    Mid Module assesment 1.0

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -2,16 +2,21 @@
 * @param {any} val - The input value
 * @returns {boolean} - Is val a number
 */
-function isNumber() {
-
+function isNumber(val) {
+if( val = number){
+  return val
+}
+console.log(isNumber(3))
 }
 
 /** Takes in a value and returns whether it is truthy
 * @param {any} val - The input value
 * @returns {boolean} - Is val truthy
 */
-function isTruthy() {
-
+function isTruthy(val) {
+if(val){
+  return val
+}
 }
 
 
--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,8 +2,13 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
+function firstAndLastAverage(arr) {
+  if(arr.length > 2 ){
+return (arr[0] + arr[arr.length-1]) /2
+  }
+  else{
+    return null
+  }
 }
 
 /** Duplicates all values inside an array.
@@ -12,16 +17,21 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+return arr.push(arr)
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
-
+function removeFirstThreeElements(arr) {
+if(arr.length > 3){
+  return arr.slice(3)
+}
+else{
+  return []
+}
 }
 
 module.exports = {
