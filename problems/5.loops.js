522b9f93c1fb2580810d96bba0bbe5dd038c15ef
Jamee Hengari <jhengari@hotmail.com>
2020-12-22 12:53:08 -0500

    First Mid moudle assessment commit

--- problems/5.loops.js
+++ problems/5.loops.js
@@ -2,18 +2,29 @@
 * @param {number} num - The number to iterate up to
 * @param {number[]} - An array of even numbers from 0 to {num}
 */
-function evenNumbers() {
-
+function evenNumbers(num) {
+let newArr= [] 
+  for (let i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      newArr.push(i)
+    }
+  } return newArr
 }
-
+console.log(evenNumbers(10))
 /** Returns all multiples of a given number up to 1000
  * Example if num was 3 the output would be [0, 3, 6, 9, 12, ...etc ] until 1000
 * @param {number} num - The number to find multiples of
 * @returns {number[]} - An array of all multiples of {num} up to 1000
 */
-function multiplesToOneThousand() {
-
+function multiplesToOneThousand(num) {
+let newArr = []
+ for (let i = 0; i <= 1000; i++) {
+   if (i % num === 0) {
+     newArr.push(i)
+   }
+ } return newArr
 }
+console.log(multiplesToOneThousand(3))
 
 /** Returns the first n powers of 2 (starting at 0)
 * Sample input: 6
@@ -22,9 +33,13 @@ function multiplesToOneThousand() {
 * @param {number} count - How many powers of two to find
 * @returns {number[]} - An array with the first {count} powers of two
 */
-function powersOfTwo() {
-
+function powersOfTwo(count) {
+let newArr = []
+ for (let i = 0; i < count; i++) {
+   newArr.push(2**i)
+ } return newArr
 }
+console.log(powersOfTwo(6))
 
 module.exports = {
   evenNumbers,
