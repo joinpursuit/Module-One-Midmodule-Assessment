81e6db7686ae94e5d787d3375a4f94e5cb37b04a
Daniel Naranjo <MR.DANIEL.NARANJO@PROTONMAIL.COM>
2020-12-22 12:43:17 -0500

    Imcomplete

--- problems/1.types+operators.js
+++ problems/1.types+operators.js
@@ -2,18 +2,49 @@
 * @param {any} val - The input value
 * @returns {boolean} - Is val a number
 */
-function isNumber() {
+function isNumber(val) {
+
+  if (typeof val === "number")
+  {
+    return true
+  }else 
+  {
+    return false
+  }
 
 }
+isNumber()
+console.log(isNumber(-5))
 
 /** Takes in a value and returns whether it is truthy
 * @param {any} val - The input value
 * @returns {boolean} - Is val truthy
 */
-function isTruthy() {
+function isTruthy(val) 
+{
+
+   let other = val
+   console.log('ingreso ' + other)
+  switch(other)
+  {
+    case typeof other === "boolean"       : return other;    break;
+    case typeof other === "number"        : return true;   break;
+    case typeof other === "string"        : return true;   break;
+    case typeof other === "undefined"     : return false;  break;
+  //   // case typeof val === ""        : return true;  break;
+  //   // case  0         : return false; break;
+  //   // case null       : return false; break;  
+  //   // case undefined  : return false; break;
+  //   // case NaN        : return false; break;
+  //   // case 'NaN'      : return false; break;
+  //   // case Infinity   : return true;  break;
+  //   // default         : return true;  break;
+  }
 
 }
 
+isTruthy()
+console.log(isTruthy(7))
 
 module.exports = {
   isNumber,
--- problems/5.loops.js
+++ problems/5.loops.js
@@ -2,18 +2,41 @@
 * @param {number} num - The number to iterate up to
 * @param {number[]} - An array of even numbers from 0 to {num}
 */
-function evenNumbers() {
+function evenNumbers(num) 
 
+{
+  let number = []
+    for (let i = 0 ; i <= num; i +=2)
+    {
+
+      number.push(i)      
+    }
+return number 
 }
+
+evenNumbers(50)
 
 /** Returns all multiples of a given number up to 1000
  * Example if num was 3 the output would be [0, 3, 6, 9, 12, ...etc ] until 1000
 * @param {number} num - The number to find multiples of
 * @returns {number[]} - An array of all multiples of {num} up to 1000
 */
-function multiplesToOneThousand() {
+function multiplesToOneThousand(num) 
+
+{
+  let number = []
+    for (let i = 0; i <=1000; i += 1)
+    {
+        if(i % num === 0)
+        {
+          number.push(i)
+        }
+    }
+return number
 
 }
+
+multiplesToOneThousand(4)
 
 /** Returns the first n powers of 2 (starting at 0)
 * Sample input: 6
@@ -22,9 +45,20 @@ function multiplesToOneThousand() {
 * @param {number} count - How many powers of two to find
 * @returns {number[]} - An array with the first {count} powers of two
 */
-function powersOfTwo() {
+function powersOfTwo(count) 
 
+{
+ let number =[]
+ let powers = 0
+  for (let i = 0; i <= count; i +=1)
+  {
+   powers = 2**i 
+   number.push(powers)
+  }
+  return number 
 }
+
+powersOfTwo(6)
 
 module.exports = {
   evenNumbers,
