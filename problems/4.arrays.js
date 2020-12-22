8fc71107dd191c17e9c885500b98a71e9a69916b
Hady Mohamed <hadymohamed@pursuit.org>
2020-12-22 12:46:36 -0500

    mid module assignment

--- problems/4.arrays.js
+++ problems/4.arrays.js
@@ -2,8 +2,13 @@
 * @param {number[]} arr - The input array
 * @returns {number} - The average of the first and last values
 */
-function firstAndLastAverage() {
-
+function firstAndLastAverage(arr) {
+if(arr.length < 2){
+  return null
+}
+else{
+  return ((arr[0] + arr[arr.length - 1]) / 2)
+}
 }
 
 /** Duplicates all values inside an array.
@@ -12,15 +17,27 @@ function firstAndLastAverage() {
 * @param {number[]} arr - The input array
 * @returns {number[]} - The duplicated array
 */
-function duplicateArray() {
-
+function duplicateArray(arr) {
+let arrDup = [...arr]
+for(let i = 0; i < arr.length; i++)
+arrDup.push(arr[i])
+return arrDup
 }
 
 /** Removes the first 3 elements from an array. If the array has fewer than 3 elements, return an empty array.
 * @param {any[]} arr - The input array
 * @returns {any[]} - The array with the first 3 elements removed
 */
-function removeFirstThreeElements() {
+function removeFirstThreeElements(arr) {
+  let arrRemove = []
+  if(arr.length < 3){
+    return arrRemove
+  }
+  else{
+    for(let i = 3; i < arr.length; i++)
+    arrRemove.push(arr[i])
+    return arrRemove
+  }
 
 }
 
